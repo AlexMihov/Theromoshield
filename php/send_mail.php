e4cc754961206772c3367534134a168adf3db116
AlexMihov <mihov.alex@gmail.com>
2014-02-08 22:51:53 +0200

    kontakt generell sends mails

--- php/send_mail.php
+++ php/send_mail.php
@@ -1,8 +1,8 @@
 <?php
 // check for form submission - if it doesn’t exist then send back to contact form
-if (!isset($_POST["save"]) || $_POST["save"] != "contact") {
-    header("Location: contact-form.php"); exit;
-}
+//if (!isset($_POST["save"]) || $_POST["save"] != "contact") {
+//    header("Location: contact-form.php"); exit;
+//}
 // get the posted data
 $name = $_POST["name"];
 $nachname = $_POST["nachname"];
@@ -10,22 +10,29 @@ $email = $_POST["email"];
 $message = $_POST["message"];
 
 //check valid email
-if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email_address))
+/*if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email_address))
     $error = "You must enter a valid email address.";
 // check that a message was entered
 elseif (emptyempty($message))
     $error = "You must enter a message.";
 // check if an error was found - if there was, send the user back to the form
 if (isset($error)) {
-    header("Location: contact-form.php?e=".urlencode($error)); exit;
-}
+    header("Location: ../contact.html?e=".urlencode($error)); exit;
+}*/
 // write the email content
-$email_content = "Vorname: ".$name;
-$email_content .= " Nachname: ". $nachname:\n
-$email_content .= "Email Addresse: " . $email_address\n;
-$email_content .= "Nachricht:" . \n\n$message;
+$email_content = "Isim: " . $name;
+$email_content .= $nachname."\n";
+$email_content .= "Email: " . $email."\n";
+$email_content .= "Mesaj:" ."\n".$message;
 // send the email
-mail ("alex@mihov.ch", "New Kontact", $email_content);
+$header = "From:". $email . "\r\n" .
+    "Reply-To:". $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+mail ("serbagrup2012@gmail.com", "Ben yeni mailim", $email_content, $header);
+mail ("hakanersoy.he@gmail.com", "Ben yeni mailim", $email_content, $header);
 // send the user back to the form
-//header("Location: contact-form.php?s=".urlencode("Thank you for your message.")); exit;
+
+
+
+header("Location: ../contact.html")); exit;
 ?>
